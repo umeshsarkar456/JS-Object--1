d3453f3d95786e2582e9396af5c55a273633f08e
umeshsarkar456 <95928010+umeshsarkar456@users.noreply.github.com>
2021-12-30 23:45:40 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,10 @@
+const shape = require("./shape")
+
+module.exports=class Circle extends shape{
+    constructor(parameters){
+        super(color);
+    }
+    calculateArea(){
+        return this.color
+    }
+}
---
+++ shape.js
@@ -0,0 +1,18 @@
+module.exports = class shape{
+    constructor(parameters){
+        this.color="red";
+    }
+    drawShape(){}
+    calculateArea(){
+        return this.color
+    }  
+
+    colorProperty(){
+        this.color = black;
+    }
+    
+}
+
+
+
+
